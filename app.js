58d112843283fcb056681ef51aa1a984786901c4
liubomyr <debryniuk@gmail.com>
2020-04-22 07:00:43 +0300

    allow origin to all hosts

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const db = require('./dataBase').getInstance();
 db.setModels();
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    // res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE,PATCH");
     res.header("Access-Control-Allow-Headers", "*");
